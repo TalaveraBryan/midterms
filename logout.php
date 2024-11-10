209dfafdfe3c07319b08a04e17a89f83636f14fa
TalaveraBryan <bryanjaytalavera22@gmail.com>
2024-11-10 22:14:59 +0800

    reverted back as it is not working as intended

--- logout.php
+++ logout.php
@@ -1,9 +1,6 @@
 <?php
-session_start();
-session_unset(); // Remove all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to login page
-header('Location: index.php');
+session_start(); // Start the session
+session_destroy(); // Destroy the session to log the user out
+header('Location: index.php'); // Redirect to the login page after logout
 exit;
 ?>
